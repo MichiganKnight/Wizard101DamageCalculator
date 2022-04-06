fb58f88ab0289146783d8118648f439591bc39cf
MichiganKnight <88414825+MichiganKnight@users.noreply.github.com>
2022-04-06 03:29:46 -0700

    Create EnchantmentWindow

---
+++ Windows/EnchantmentWindow.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Wizard101DamageCalculator.Windows
+{
+    partial class EnchantmentWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureStrong = new System.Windows.Forms.PictureBox();
+            this.PictureGiant = new System.Windows.Forms.PictureBox();
+            this.PictureMonstrous = new System.Windows.Forms.PictureBox();
+            this.PictureGargantuan = new System.Windows.Forms.PictureBox();
+            this.PictureColossal = new System.Windows.Forms.PictureBox();
+            this.PictureEpic = new System.Windows.Forms.PictureBox();
+            this.PictureSpell = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureStrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureGiant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureMonstrous)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureGargantuan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureColossal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureEpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureSpell)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureStrong
+            // 
+            this.PictureStrong.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureStrong.Image = global::Wizard101DamageCalculator.Properties.Resources.Strong;
+            this.PictureStrong.Location = new System.Drawing.Point(186, 12);
+            this.PictureStrong.Name = "PictureStrong";
+            this.PictureStrong.Size = new System.Drawing.Size(127, 195);
+            this.PictureStrong.TabIndex = 4;
+            this.PictureStrong.TabStop = false;
+            this.PictureStrong.Click += new System.EventHandler(this.PictureStrong_Click);
+            // 
+            // PictureGiant
+            // 
+            this.PictureGiant.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureGiant.Image = global::Wizard101DamageCalculator.Properties.Resources.Giant;
+            this.PictureGiant.Location = new System.Drawing.Point(355, 12);
+            this.PictureGiant.Name = "PictureGiant";
+            this.PictureGiant.Size = new System.Drawing.Size(127, 195);
+            this.PictureGiant.TabIndex = 5;
+            this.PictureGiant.TabStop = false;
+            this.PictureGiant.Click += new System.EventHandler(this.PictureGiant_Click);
+            // 
+            // PictureMonstrous
+            // 
+            this.PictureMonstrous.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureMonstrous.Image = global::Wizard101DamageCalculator.Properties.Resources.Monstrous;
+            this.PictureMonstrous.Location = new System.Drawing.Point(524, 12);
+            this.PictureMonstrous.Name = "PictureMonstrous";
+            this.PictureMonstrous.Size = new System.Drawing.Size(127, 195);
+            this.PictureMonstrous.TabIndex = 6;
+            this.PictureMonstrous.TabStop = false;
+            this.PictureMonstrous.Click += new System.EventHandler(this.PictureMonstrous_Click);
+            // 
+            // PictureGargantuan
+            // 
+            this.PictureGargantuan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureGargantuan.Image = global::Wizard101DamageCalculator.Properties.Resources.Gargantuan;
+            this.PictureGargantuan.Location = new System.Drawing.Point(693, 12);
+            this.PictureGargantuan.Name = "PictureGargantuan";
+            this.PictureGargantuan.Size = new System.Drawing.Size(127, 195);
+            this.PictureGargantuan.TabIndex = 7;
+            this.PictureGargantuan.TabStop = false;
+            this.PictureGargantuan.Click += new System.EventHandler(this.PictureGargantuan_Click);
+            // 
+            // PictureColossal
+            // 
+            this.PictureColossal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureColossal.Image = global::Wizard101DamageCalculator.Properties.Resources.Colossal;
+            this.PictureColossal.Location = new System.Drawing.Point(862, 12);
+            this.PictureColossal.Name = "PictureColossal";
+            this.PictureColossal.Size = new System.Drawing.Size(127, 195);
+            this.PictureColossal.TabIndex = 8;
+            this.PictureColossal.TabStop = false;
+            this.PictureColossal.Click += new System.EventHandler(this.PictureColossal_Click);
+            // 
+            // PictureEpic
+            // 
+            this.PictureEpic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureEpic.Image = global::Wizard101DamageCalculator.Properties.Resources.Epic;
+            this.PictureEpic.Location = new System.Drawing.Point(1031, 12);
+            this.PictureEpic.Name = "PictureEpic";
+            this.PictureEpic.Size = new System.Drawing.Size(127, 195);
+            this.PictureEpic.TabIndex = 9;
+            this.PictureEpic.TabStop = false;
+            this.PictureEpic.Click += new System.EventHandler(this.PictureEpic_Click);
+            // 
+            // PictureSpell
+            // 
+            this.PictureSpell.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureSpell.Location = new System.Drawing.Point(17, 12);
+            this.PictureSpell.Name = "PictureSpell";
+            this.PictureSpell.Size = new System.Drawing.Size(127, 195);
+            this.PictureSpell.TabIndex = 10;
+            this.PictureSpell.TabStop = false;
+            this.PictureSpell.Click += new System.EventHandler(this.PictureSpell_Click);
+            // 
+            // EnchantmentWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1171, 210);
+            this.ControlBox = false;
+            this.Controls.Add(this.PictureSpell);
+            this.Controls.Add(this.PictureEpic);
+            this.Controls.Add(this.PictureColossal);
+            this.Controls.Add(this.PictureGargantuan);
+            this.Controls.Add(this.PictureMonstrous);
+            this.Controls.Add(this.PictureGiant);
+            this.Controls.Add(this.PictureStrong);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "EnchantmentWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enchantment Window";
+            this.Load += new System.EventHandler(this.EnchantmentWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureStrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureGiant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureMonstrous)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureGargantuan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureColossal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureEpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureSpell)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox PictureStrong;
+        private PictureBox PictureGiant;
+        private PictureBox PictureMonstrous;
+        private PictureBox PictureGargantuan;
+        private PictureBox PictureColossal;
+        private PictureBox PictureEpic;
+        private PictureBox PictureSpell;
+    }
+}
---
+++ Windows/EnchantmentWindow.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Resources;
+
+using Wizard101DamageCalculator.Properties;
+
+namespace Wizard101DamageCalculator.Windows
+{
+    public partial class EnchantmentWindow : Form
+    {
+        private readonly Dictionary<string, Spell> SpellNamePair = new();
+
+        private Spell? currentSpell;
+
+        private readonly string currentSpellName;
+
+        public EnchantmentWindow(string text)
+        {
+            InitializeComponent();
+
+            currentSpellName = text;
+        }
+
+        private void EnchantmentWindow_Load(object sender, EventArgs e)
+        {
+            Spell[] spells = Spell.GetAllSpells();
+
+            foreach (Spell spell in spells)
+            {
+                SpellNamePair.Add(spell.SpellName, spell);
+            }
+
+            foreach (KeyValuePair<string, Spell> keyValuePair in SpellNamePair)
+            {
+                currentSpell = keyValuePair.Value;
+
+                if (keyValuePair.Key == currentSpellName)
+                {
+                    School school = Spell.GetCurrentSpellSchool(keyValuePair.Value);
+                    string simpleSpell = keyValuePair.Key.Replace(" ", "");
+
+                    ResourceSet? resourceSet = Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+
+                    foreach (DictionaryEntry resource in resourceSet)
+                    {
+                        if (simpleSpell == resource.Key.ToString())
+                        {
+                            PictureSpell.Image = (Image?)resource.Value;
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PictureSpell_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureStrong_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureGiant_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureMonstrous_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureGargantuan_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureColossal_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureEpic_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
