0f1819d6cb536a040ce60fe6ddc629a15cc06390
MichiganKnight <88414825+MichiganKnight@users.noreply.github.com>
2022-04-06 12:26:04 -0700

    Update Resources

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Wizard101DamageCalculator.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FireCatStrong {
+            get {
+                object obj = ResourceManager.GetObject("FireCatStrong", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Gargantuan {
             get {
                 object obj = ResourceManager.GetObject("Gargantuan", resourceCulture);
